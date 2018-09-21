345118049dd430008784f3dad383e179148d2183
Fachruzi Ramadhan <mabuak@live.com>
2018-09-21 22:36:10 +0700

    Convert To MVC design pattern

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Fachruzi Ramadhan on 21/09/2018.
+//  Copyright © 2018 Fachruzi Ramadhan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
